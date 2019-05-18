4482f1510043d9c849cd67dec7a8da45ef5a204c
fernandolucas <fernandolucass.2012@gmail.com>
2019-05-18 16:48:18 -0300

    structing project

--- ProAgil.Domain/RedeSocial.cs
+++ ProAgil.Domain/RedeSocial.cs
@@ -6,8 +6,8 @@ namespace ProAgil.Domain
         public string Nome { get; set; }
         public string URL { get; set; }
         public int? EventoId { get; set; }
+        public Evento Evento { get; set; }  
         public int? PalestranteId { get; set; }
-        public Evento Eventos { get; set; }  
-        public Palestrante Palestrantes { get; set; }
+        public Palestrante Palestrante { get; set; }
     }
 }
--- ProAgil.WebAPI/Startup.cs
+++ ProAgil.WebAPI/Startup.cs
@@ -6,12 +6,14 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using ProAgil.WebAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+
+using ProAgil.Repository;
 
 namespace ProAgil.WebAPI
 {
@@ -28,10 +30,10 @@ namespace ProAgil.WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             //injeta o DataContext na Controller
-            services.AddDbContext<DataContext>( 
+            services.AddDbContext<ProAgilContext>(
                 x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection"))
             );
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddCors();
         }
 
@@ -44,15 +46,13 @@ namespace ProAgil.WebAPI
             }
             else
             {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
-            //Não vou usar por enquanto
+            //TODO: Não vou usar por enquanto
             //app.UseHttpsRedirection();
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-
-            //Encontrar imagens que estão no wwwroot
-            app.UseStaticFiles();
             app.UseMvc();
         }
     }
